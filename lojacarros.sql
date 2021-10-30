25d7bb60018599ce405275a41c1092f93253b0a8
milenavidaal <85593691+milenavidaal@users.noreply.github.com>
2021-10-30 19:46:00 -0400

    Add files via upload

---
+++ lojacarros.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 30-Out-2021 às 23:39
+-- Versão do servidor: 5.7.31
+-- versão do PHP: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `lojacarros`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoque`
+--
+
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `modelo` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `descricao` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `modelo_fab` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `cor` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `placa` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `valor` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `estoque`
+--
+
+INSERT INTO `estoque` (`id`, `marca`, `modelo`, `descricao`, `modelo_fab`, `cor`, `placa`, `valor`) VALUES
+(2, 'Ford', 'Ka', '1.0 MPI 8V FLEX 2P MANUAL', '2013/2013', 'Branco', 'GHF9845', 18000),
+(11, 'aff', '', '', '2014/2004', '', '', 12000),
+(5, 'Toyota', 'Corola', '1.8 SE-G 16V FLEX 4P AUTOMÁTICO', '2007/2008', 'Preto', 'KZY4838', 38000),
+(6, 'RAM', 'DODGE RAM 2500', 'Ram 2500 CD 6.7 4X4 Night Edition', '', 'Preta', 'KRA5847', 404000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
